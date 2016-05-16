5866eeeea400081d1698ed623d126dae4b3fe7ad
Vinod H I <vinod@lifetape.com>
2016-05-16 12:08:00 +0530

    credentials

--- server/main.js
+++ server/main.js
@@ -1,5 +1,11 @@
-import { Meteor } from 'meteor/meteor';
+Meteor.startup(function() {
+  ServiceConfiguration.configurations.remove({
+    service: 'google'
+  });
 
-Meteor.startup(() => {
-  // code to run on server at startup
-});
+  ServiceConfiguration.configurations.insert({
+      service: 'google',
+      clientId: '977615698918-5qs0n0f33nts3m1v95944f52i3tkleeu.apps.googleusercontent.com',
+      secret: 'lsqJ7YWu7mbVkaxCw_RgqRJU'
+  });
+})
